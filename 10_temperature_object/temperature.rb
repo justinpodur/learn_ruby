84c0988a1dd2e30fb4be3869e34f5cf2db65c73b
git-justinpodur <git-justin@podur.org>
2014-06-16 17:07:17 -0400

    followed arjun's solution for 10

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,56 +1,59 @@
 class Temperature
-  attr_reader :f, :c
-  alias_method :in_fahrenheit, :f
-  alias_method :in_celsius, :c
+attr_accessor :f, :c
+#attrs means attributes
+def initialize(attrs = Hash.new(0))
+	if attrs[:f]
+		@f = attrs[:f]
+		@c = Temperature.ftoc(attrs[:f])
+	elsif attrs[:c]
+		@c = attrs[:c]
+		@f = Temperature.ctof(attrs[:c])
+	end
+end
 
-  def initialize(attrs = Hash.new(0))
-    if attrs[:f]
-      @f = attrs[:f]
-      @c = Temperature.ftoc(attrs[:f])
-    elsif attrs[:c]
-      @c = attrs[:c]
-      @f = Temperature.ctof(attrs[:c])
-    end
-  end
+def self.in_celsius(c)
+	self.new(:c => c)
+#basically calling your initializer again
+end
 
-  def f=(temperature)
-    @f = temperature
-    @c = Temperature.ftoc(@f)
-  end
+def self.in_fahrenheit(f)
+	self.new(:f => f)
+end
 
-  def c=(temperature)
-    @c = temperature
-    @f = Temperature.ctof(@c)
-  end
+def to_celsius
+	@c.to_i
+end
 
-  def self.ftoc(f)
-    (f - 32) * (5.0 / 9.0)
-  end
+def to_fahrenheit
+	@f.to_i
+end
 
-  def self.ctof(c)
-    c * (9.0 / 5.0) + 32
-  end
+def self.ftoc(f)
+	(f-32)*(5.0/9)
+end
 
-  def self.to_fahrenheit(temperature)
-    Temperature.new(:c => temperature)
-  end
-
-  def self.to_celsius(temperature)
-    Temperature.new(:f => temperature)
-  end
+def self.ctof(c)
+	c*(9.0/5)+32
+end
 
 end
 
 class Celsius < Temperature
-  def initialize(degrees)
-    @c = degrees
-    @f = Celsius.ctof(degrees)
-  end
+	def initialize(c)
+		@c = c
+	end
+
+	def to_fahrenheit
+		@c*(9.0/5)+32
+	end
 end
 
 class Fahrenheit < Temperature
-  def initialize(degrees)
-    @f = degrees
-    @c = Fahrenheit.ftoc(degrees)
-  end
+	def initialize(f)
+		@f = f
+	end
+
+	def to_celsius
+		(@f-32)*(5.0/9)
+	end
 end
